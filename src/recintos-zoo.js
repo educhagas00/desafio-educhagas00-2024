643c159be308dfab9dc7babcdc1627f98d09e621
educhagas00 <educhagas00@gmail.com>
2024-09-09 20:04:35 -0300

    fix: correção de leitura em array

--- src/recintos-zoo.js
+++ src/recintos-zoo.js
@@ -5,7 +5,7 @@ class RecintosZoo {
             { numero: 1, bioma: ['savana'], tamanho_total: 10, animais:[{especie: 'MACACO', quantidade: 3}]},
             { numero: 2, bioma: ['floresta'], tamanho_total: 5, animais: [] },
             { numero: 3, bioma: ['savana', 'rio'], tamanho_total: 7, animais:[{especie: 'GAZELA', quantidade: 1}]},
-            { numero: 4, bioma: ['rio'], tamanho_total: 8, animais:[] },
+            { numero: 4, bioma: ['rio'], tamanho_total: 8, animais: [] },
             { numero: 5, bioma: ['savana'], tamanho_total: 9, animais:[{especie: 'LEAO', quantidade: 1}]}
         ];
 
@@ -42,9 +42,9 @@ class RecintosZoo {
             return { erro: "Quantidade inválida" }; 
         }
 
-        const RecintosViaveis = [];
+        const recintosViaveis = [];
         const listaAnimal = this.animais[animal]; // informação do animal
-        const espacoNecessario = listaAnimal.tamanho * quantidade; // espaço que animal ocupa
+        let espacoNecessario = listaAnimal.tamanho * quantidade; // espaço que animal ocupa
 
         
         //this.recintos[tamanho_total] - this.recintos[animais.quantidade] * listaAnimal.this.recintos.animais.especie;
@@ -54,35 +54,44 @@ class RecintosZoo {
 
             // espaço ocupado pelos animais ja inseridos no recinto
             const espacoOcupado = recinto.animais.reduce((total, atual) => {
-                return total + this.animais[atual.especie].tamanho * atual.quantidade; 
+                return total + (this.animais[atual.especie].tamanho * atual.quantidade); 
             }, 0);
 
             // espaço livre pra inserção de animais
-            const espacoDisponivel = recinto.tamanho - espacoOcupado;
-
-            let espacoSuficiente = espacoDisponivel >= espacoNecessario; // se da pra encaixar animal ou nao
+            const espacoDisponivel = recinto.tamanho_total - espacoOcupado;
+            
+            // se da pra encaixar animal ou nao
+            let espacoSuficiente = espacoDisponivel >= espacoNecessario; 
 
             if((recinto.animais.length > 0) && (recinto.animais.some(a => a.especie !== animal)))
             {
-                espacoSuficiente = espacoDisponivel >= espacoNecessario + 1;
+                espacoNecessario += 1;
+                espacoSuficiente = espacoDisponivel >= espacoNecessario;
             }
 
 
-            if(espacoSuficiente) {
-                RecintosViaveis.push(`Recinto ${recinto.numero} (espaço livre: ${espacoDisponivel} total ${recinto.tamanho_total}`);
-            }
+            //animais apenas no bioma apropriadoec
+            const biomaApropriado = recinto.bioma.some(b => listaAnimal.bioma.includes(b));
 
+
+
+            if(espacoSuficiente && biomaApropriado) {
+                recintosViaveis.push(`Recinto ${recinto.numero} (espaço livre: ${espacoDisponivel} total ${recinto.tamanho_total}`);
+            }
             
-            
+     
         });
 
-        if(RecintosViaveis.length === 0) {
+        if(recintosViaveis.length === 0) {
             return { erro: "Não há recinto viável" };
-        } 
+        }
 
-        return { RecintosViaveis };
+        return { recintosViaveis };
+
+       
 
     }
+    
 
 }
 
